03afdb6968ffc7e7c8e1c82b927e3987092d4a9d
vatch123 <vatsalyachaubey19980@gmail.com>
2020-09-10 13:33:12 +0530

    added line arguments

--- system.py
+++ system.py
@@ -7,13 +7,14 @@ import sender
 import receiver
 import random
 from channel_erasure import bernouli_erasure, gilbert_elliot_erasure
+import argparse
 
 def system(
     length = 8,
     number = 10000,
     size = 4,
     delay_tolerance = 16,
-    feedback_interval = 20,
+    feedback_interval = 1,
     channel_erasure_model = 'Gilbert-Elliot',
     Pbg = 0.5,
     Pgb = 0.4,
@@ -27,8 +28,8 @@ def system(
     delay_tolerance: No. of previous packets of interest at the current moment
     feedback_interval: Feedback sent after every these many packets
     channel_erasure_model: Type of channel erasure
-    erasure_prob: Probabiltiy of packet lost in Bernouli/Gilbert
-    erasure_prob2: Second probability in Gilbert
+    Pbg: Probability of transition from bad to good
+    Pgb: Probability of transition from good to bad
     erasure_prob_feedback: Feedback erasure probability
     """
 
@@ -50,8 +51,8 @@ def system(
         if channel_erasure_model == 'Bernouli':
             lost = bernouli_erasure(Pbg)
         elif channel_erasure_model == 'Gilbert-Elliot':
-            previous_status = gateway.received_packet_list[i-1] if i>=0 else False
-            lost = gilbert_elliot_erasure( previous_status, Pbg, Pgb)
+            previous_status = gateway.received_packet_list[i-1] if i>=0 else True
+            lost = gilbert_elliot_erasure(previous_status, Pbg, Pgb)
 
         # Receiver's side
         gateway.update_packet_reception(i, lost)
@@ -94,4 +95,29 @@ def system(
 
 
 if __name__=="__main__":
-    system(channel_erasure_model='Bernouli')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-l', '--length', help='Length of one message in binary', type=int, default=8)
+    parser.add_argument('-n', '--number', help='Number of messages to transmit', type=int, default=10000)
+    parser.add_argument('-c', '--channel', help='The channel erasure model', choices=['Bernouli', 'Gilbert-Elliot'], type=str, default='Bernouli')
+    parser.add_argument('-s', '--size', help='Size of the packet', type=int, default=4)
+    parser.add_argument('-d', '--delay_tolerance', help='Delay Tolerance', type=int, default=16)
+    parser.add_argument('--pbg', help='Channel Erasure Probability for Bernouli or Probability of good to bad transition in Gilbert Elliot', type=float, default=0.5)
+    parser.add_argument('--pgb', help='Probability of bad to good transition in Gilbert Elliot', type=float, default=0.4)
+    parser.add_argument('-f', '--feedback_erasure', help='Feedback erasure probability', type=float, default=0.4)
+    parser.add_argument('-i', '--feedback_interval', help='The interval of sending feedback', type=int, default=1)
+    parser.add_argument('--scheme', help='The coding scheme to use', choices=['ICC', 'repetition'], type=str, default='ICC')
+    args = parser.parse_args()
+
+    system(
+        length = args.length,
+        number = args.number,
+        size = args.size,
+        delay_tolerance = args.delay_tolerance,
+        feedback_interval = args.feedback_interval,
+        channel_erasure_model = args.channel,
+        Pbg = args.pbg,
+        Pgb = args.pgb,
+        erasure_prob_feedback = args.feedback_erasure,
+        scheme = args.scheme
+    )
+
