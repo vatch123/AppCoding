8e5727c43eb55ecf1a3c06dedf404e6e467f3b86
vatch123 <vatsalyachaubey19980@gmail.com>
2020-09-10 13:32:22 +0530

    Fixed gilbert elliot

--- channel_erasure.py
+++ channel_erasure.py
@@ -14,11 +14,11 @@ def gilbert_elliot_erasure(previous_status, Pbg, Pgb):
     """
 
     # Check if packet_number-1 was lost
-    if previous_status:
+    if not previous_status:
         x = np.random.uniform()
-        return True if x < Pbg else False
+        return True if x < 1 - Pbg else False
     
     else:
         x = np.random.uniform()
-        return True if x < 1 - Pgb else False
+        return True if x < Pgb else False
 
